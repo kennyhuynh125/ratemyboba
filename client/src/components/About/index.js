68ede6464e5740fda9a00bfc6340f7ea95b3e74a
Kenny Huynh <huynhkenny125@yahoo.com>
2018-05-22 19:51:54 -0700

    made style changes

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -9,18 +9,17 @@ class About extends React.Component {
 				<Navbar auth={auth} />
 				<div className="container">
 					<div>
-						<h1>About</h1>
-						<p>Think about RateMyProfessor, but for boba</p>
+						<h1>RateMyProfessor, but for boba.</h1>
 					</div>
 					<div>
 						<h1>Inspiration</h1>
 						<p>To create a community where users can share how they like their boba and for the public to go and give it a try.</p>
-						<p>On Yelp, many post about the service, not actually the drink itself. If it is about the drink, many just say what drink they bought.
+						<p>On Yelp, many post about the service, not actually about the drink itself. If it is about the drink, most just say what drink they bought.
 						This is okay, but I wanted to dive deeper. Many stores give the option to change sweetness, ice, and even tea levels of the drink. This
 						plays a huge part in determining how good the drink is towards the buyer. Many people have different taste buds; some like sweeter drinks
 						and some like bitter drinks. This site was created for this purpose. Users have the ability to post their drinks and the many different options
 						(sweetness level, ice level, toppings, more to come) that a drink can have. Not only that, users can add a small description of the drink that can
-						be used to write something such as recommendations. You guys decide :) </p>
+						be used to write something such as recommendations. You guys decide :)</p>
 					</div>
 				</div>
 			</div>
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 import Navbar from '../Navbar';
 import BobaPost from '../BobaPost';
 
+import './style.css';
+
 class Home extends React.Component {
 	constructor(props) {
 		super(props);
@@ -36,23 +38,27 @@ class Home extends React.Component {
 		return (
 			<div>
 				<Navbar auth={auth} />
-				<h1>Rate My Boba</h1>
-				<p>RateMyProfessor, but for Boba.</p>
-				<h1>Boba Drinks</h1>
-					<div>
+				<div className="body">
+					<div className="header">
+						<h1>Rate My Boba</h1>
+						<p>RateMyProfessor, but for Boba.</p>
+						<h1>Current Bobas</h1>
+					</div>
+					<div className="boba-posts">
 					{
 						this.state.bobas.map((boba, i) => {
 							return <BobaPost
-									key={i}
-									name={boba.name}
-									ice={boba.ice}
-									shop={boba.shop}
-									sweetness={boba.sweetness}
-									toppings={boba.toppings}
-									description={boba.description} />
+								key={i}
+								name={boba.name}
+								ice={boba.ice}
+								shop={boba.shop}
+								sweetness={boba.sweetness}
+								toppings={boba.toppings}
+								description={boba.description} />
 						})
 					}
 					</div>
+				</div>
 			</div>
 		)
 	}
